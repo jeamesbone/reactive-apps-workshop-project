3eb01ae405b04991796e6628477c3e84df0d0a93
Jeames Bone <jeames.bone@gmail.com>
2017-08-26 10:17:35 +1000

    Initial Commit

---
+++ ReactiveAppsWorkshop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReactiveAppsWorkshop
+//
+//  Created by Jeames Bone on 26/8/17.
+//  Copyright © 2017 Jeames Bone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
